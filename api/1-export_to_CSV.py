9f806542ebd14d9ddb303d6d05bf0b9dfee7d21f
hd77alu <h.abdalgade@alustudent.com>
2025-04-17 22:05:08 +0200

    csv task

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,45 +1,24 @@
 #!/usr/bin/python3
-""" Rest API script that gathers data from an API and saves it to a CSV. """
+""" Export api to csv"""
 import csv
-import json
+import requests
 import sys
-import urllib
-import urllib.request
 
-# The base API url for getting the employee object
-USER_API_URL = "https://jsonplaceholder.typicode.com/users/"
+if __name__ == '__main__':
+    user = sys.argv[1]
+    url_user = 'https://jsonplaceholder.typicode.com/users/' + user
+    res = requests.get(url_user)
+    """ANYTHING"""
+    user_name = res.json().get('username')
+    task = url_user + '/todos'
+    res = requests.get(task)
+    tasks = res.json()
 
-# The base API url for getting all todo objects for an employee
-TODO_API_URL = "https://jsonplaceholder.typicode.com/todos?userId="
-
-# Employee ID passed as an argument to the script
-emp_id: str = sys.argv[1] if len(sys.argv) > 1 else ""
-
-# Get all the todos for a given employee ID
-if emp_id.isdigit():
-    try:
-        user_url = f"{USER_API_URL}{emp_id}"
-        todos_url = f"{TODO_API_URL}{emp_id}"
-
-        emp_response = urllib.request.urlopen(user_url)
-        todos_response = urllib.request.urlopen(todos_url)
-
-        emp_data = emp_response.read()
-        todos_data = todos_response.read()
-
-        employee = json.loads(emp_data)
-        todos = json.loads(todos_data)
-
-        username = employee.get("username")
-
-        with open(f"{emp_id}.csv", "w", newline="") as csvfile:
-            writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-            for todo in todos:
-                todo_status = todo.get("completed")
-                todo_title = todo.get("title")
-                writer.writerow([emp_id, username, todo_status, todo_title])
-
-    except urllib.error.URLError as err:
-        print(f"An error occurred: {err}")
-    except json.JSONDecodeError as err:
-        print(f"Error decoding JSON: {err}")
+    with open('{}.csv'.format(user), 'w') as csvfile:
+        for task in tasks:
+            completed = task.get('completed')
+            """Complete"""
+            title_task = task.get('title')
+            """Done"""
+            csvfile.write('"{}","{}","{}","{}"\n'.format(
+                user, user_name, completed, title_task))
